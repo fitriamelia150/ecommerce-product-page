4809a315c023c525b28f2d19d64ca17ba73e8af1
fitria <user76@realtechltd.com>
2025-01-10 17:33:53 +0700

    initate project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <p className="font-bold ">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
